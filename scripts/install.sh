84f134426fbf6a7efd557c591241e27fac326c69
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-06-06 21:51:54 +0100

    Update Commit - 06/06/2019 - Update Travis CI Integracion 21

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,9 +2,8 @@
 
 set -x
 
-openssl aes-256-cbc -K $encrypted_b525089bd38a_key -iv $encrypted_b525089bd38a_iv -in deploy-travis.enc -out deploy-travis -d && \
+openssl aes-256-cbc -K $encrypted_b525089bd38a_key -iv $encrypted_b525089bd38a_iv -in deploy-travis.enc -out ~/.ssh/deploy-travis -d && \
 
 rm deploy-travis.enc && \
-chmod 600 deploy-travis && \
-mv deploy-travis ~/.ssh/id_rsa
- 
+chmod 600 ~/.ssh/deploy-travis && \
+mv ~/.ssh/deploy-travis ~/.ssh/id_rsa
