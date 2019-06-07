24bc436fd284e5226abc4b45ce75496ffe8cb73d
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-06-07 03:45:32 +0100

    Update Commit - 06/06/2019 - Update Travis CI Integracion 36

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,6 @@
 
 # scp <PATH_FILE> ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
 
-ssh -o StrictHostKeyChecking=no -i deploy-travis -vp ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} "cd ${REMOTE_PATH} && pwd"
+COMMAND="cd ${REMOTE_PATH} && touch tutu.txt"
+
+ssh -o StrictHostKeyChecking=no -i deploy-travis -vp ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
