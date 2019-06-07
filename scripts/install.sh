f21ce0c642fb46a93ea97617228e3094e7fdb441
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-06-07 02:43:59 +0100

    Update Commit - 06/06/2019 - Update Travis CI Integracion 25

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,8 +4,6 @@ echo "Importing SSH Key."
 
 openssl aes-256-cbc -K $encrypted_b525089bd38a_key -iv $encrypted_b525089bd38a_iv -in deploy-travis.enc -out deploy-travis -d
 
-rm deploy-travis.enc
-
 chmod 600 deploy-travis
 
-mv deploy-travis ~/.ssh/id_rsa
+ssh-add deploy-travis
