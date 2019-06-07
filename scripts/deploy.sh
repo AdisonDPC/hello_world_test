e5d56a901bb8222c1196f1d0b1a7c3b5a88ba0dd
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-06-07 03:42:27 +0100

    Update Commit - 06/06/2019 - Update Travis CI Integracion 35

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# tar -czf package.tgz build && \
+# scp <PATH_FILE> ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
 
-# scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH && \
-
-# ssh -vp $REMOTE_PORT $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
-
-# ssh -vp $REMOTE_PORT $REMOTE_USER@$REMOTE_HOST 'bash -s ls'
-
-ssh -o StrictHostKeyChecking=no -i deploy-travis -vp $REMOTE_PORT $REMOTE_USER@$REMOTE_HOST "cd REMOTE_PATH && pwd"
+ssh -o StrictHostKeyChecking=no -i deploy-travis -vp ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} "cd ${REMOTE_PATH} && pwd"
