a08ca3185737ad2683e314e1192241029df1b38d
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-06-07 10:20:34 +0100

    Update Commit - 07/06/2019 - Update index.html file 02.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,10 @@
 
 echo "Importing SSH Key."
 
+echo "REMOTE_PORT = ${REMOTE_PORT}"
+echo "REMOTE_USER = ${REMOTE_USER}"
+echo "REMOTE_HOST = ${REMOTE_HOST}"
+
 openssl aes-256-cbc -K $encrypted_b525089bd38a_key -iv $encrypted_b525089bd38a_iv -in deploy-travis.enc -out deploy-travis -d
 
 eval "$(ssh-agent -s)"
