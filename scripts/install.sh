e45c6248f57a7fe3c4abccba004982968c5b2daf
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-06-07 03:02:21 +0100

    Update Commit - 06/06/2019 - Update Travis CI Integracion 28

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,10 +4,10 @@ echo "Importing SSH Key."
 
 openssl aes-256-cbc -K $encrypted_b525089bd38a_key -iv $encrypted_b525089bd38a_iv -in deploy-travis.enc -out deploy-travis -d
 
-eval "$(ssh-agent -s)"
-
 chmod 600 deploy-travis
 
 echo -e "Host $REMOTE_HOST\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
+eval "$(ssh-agent -s)"
 
 ssh-add deploy-travis
