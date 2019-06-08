3b4fdffd264ff6ae80cbb1f6ae4dedc7d417988f
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-06-08 08:40:59 +0100

    Update Commit - 06/06/2019 - Update Travis CI Integracion GH-Pages 10

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# scp <PATH_FILE> ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
-
-COMMAND="cd ${REMOTE_PATH} && git pull"
-
-ssh -o StrictHostKeyChecking=no -i deploy-travis -vp ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
