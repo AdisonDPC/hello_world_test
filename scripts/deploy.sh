629e63663a4c9f907c4139860d6db5cae4fb4adf
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-06-17 22:42:57 +0100

    Update Commit - 17/06/2019 - Update Files 06.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Using Parcel (Deploy Script in Digital Ocean)."
+
 # scp <PATH_FILE> ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
 
-COMMAND="cd ${REMOTE_PATH} && git pull"
+# COMMAND="cd ${REMOTE_PATH} && git pull"
 
-ssh -o StrictHostKeyChecking=no -i deploy-travis -vp ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
+# ssh -o StrictHostKeyChecking=no -i deploy-travis -vp ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
