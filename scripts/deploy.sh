ca54fb604e937c6c77882465dc8eb447cb45b423
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-06-29 15:08:08 +0100

    Update Commit - 29/06/2019 - Update Config Files 04.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ echo "Using Parcel (Deploy Script in Digital Ocean)."
 
 # scp <PATH_FILE> ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
 
-COMMAND="cd ${REMOTE_PATH} && git pull && cp build_do/* /var/www/html/HTML/HTML_ADPCProjects_V1/"
+COMMAND="cd ${REMOTE_PATH} && git checkout master && git pull && git checkout build_gh && git pull && cp build_do/* /var/www/html/HTML/HTML_ADPCProjects_V1/"
 
 ssh -o StrictHostKeyChecking=no -i deploy-travis -vp ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
