233bc0d29df0b48adc03ae8905beaa7121b1f66d
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-06-29 15:13:43 +0100

    Update Commit - 29/06/2019 - Update Config Files 05.

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,8 @@ echo "REMOTE_PORT = ${REMOTE_PORT}"
 echo "REMOTE_USER = ${REMOTE_USER}"
 echo "REMOTE_HOST = ${REMOTE_HOST}"
 
+# cat ~/.ssh/deploy-travis.pub | ssh -vp ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} "mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys"
+
 openssl aes-256-cbc -K $encrypted_b525089bd38a_key -iv $encrypted_b525089bd38a_iv -in deploy-travis.enc -out deploy-travis -d
 
 eval "$(ssh-agent -s)"
